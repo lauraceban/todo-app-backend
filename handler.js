c98cb7056e771800fbde8ebe3d117d597889549f
lauraceban <lauracristina.ceban@gmail.com>
2020-01-29 21:56:33 +0000

    second commit

--- handler.js
+++ handler.js
@@ -2,92 +2,51 @@ const express = require("express");
 const cors = require("cors");
 const serverlessHttp = require("serverless-http");
 const bodyParser = require("body-parser"); 
+const mysql = require("mysql");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,   
+  database: "to_do_list"
+});
+
+
+
 //GET 
 app.get("/tasks", function (request, response) {
   
-  response.status(200).json({
-    tasks: [
-      {
-        name: "Do the Christmas shopping",
-        date: "2019-01-12",
-        isCompleted: false,
-        isDeleted: false,
-        id: 1
-      }, 
-      {
-        name: "Attend medical appointment",
-        date: "2019-01-12",
-        isCompleted: false,
-        isDeleted: false,
-        id: 2
-      }, 
-      {
-        name: "Apply for travelling Visa",
-        date: "2019-01-12",
-        isCompleted: false,
-        isDeleted: false,
-        id: 3
-      }, 
-      {
-        name: "Attend the opera performance",
-        date: "2019-01-12",
-        isCompleted: false,
-        isDeleted: false,
-        id: 4
-      }, 
-      {
-        name: "Choose activities for each honeymoon destination",
-        date: "2019-01-12",
-        isCompleted: false,
-        isDeleted: false,
-        id: 5
-      }, 
-      {
-        name: "Submit honeymoon requests to travel concierge",
-        date: "2019-01-12",
-        isCompleted: false,
-        isDeleted: false,
-        id: 6
-      }, 
-      {
-        name: "Study",
-        date: "2019-01-12",
-        isCompleted: true,
-        isDeleted: false,
-        id: 7
-      }, 
-      {
-        name: "Complete homework",
-        date: "2019-01-12",
-        isCompleted: true,
-        isDeleted: false,
-        id: 8
-      }, 
-      {
-        name: "Submit homework",
-        date: "2019-01-12",
-        isCompleted: true,
-        isDeleted: false,
-        id: 9
-      }
-    ]
-  });
+  connection.query(`SELECT * FROM List_of_tasks`, function (err, data) {
+    if (err) {
+      response.status(500).json({error: err});
+    } else {
+      response.status(200).json({"to_do_list": data});
+    }
+  });  
 });
+
+
 
 //POST 
 app.post("/newTask", function (request, response) {
 
   const newTask = request.body;
  
-  response.status(200).json({
-      message: `Received a request to add ${newTask.name} with date ${newTask.date}.`
+  connection.query(`INSERT INTO List_of_tasks SET ?`, [newTask], function (err, data) {
+    if (err) {
+      response.status(500).json({error: err});
+    } else {
+      newTask.id = data.insertId;
+      response.status(201).json(newTask);
+    }
   }); 
 }); 
+
+
 
 
 //PUT 
@@ -96,22 +55,32 @@ app.put("/tasks/:id", function(request, response) {
   const updatedTasks = request.body;
   const id = request.params.id;
  
-  response.status(200).json({ 
-    message: `Successfully update task ID ${id} with name: ${updatedTasks.name}, date ${updatedTasks.date}, isCompleted: ${updatedTasks.isCompleted}, isDeleted: ${updatedTasks.isDeleted}`
+  connection.query(`UPDATE List_of_tasks SET ? WHERE id=?`, [updatedTasks, id], 
+  function (err) {
+    if (err) {
+      response.status(500).json({error: err});
+    } else {
+      response.sendStatus(200);
+    }
   });
 });
+
 
 
 //DELETE 
 app.delete("/tasks/:id", function (request, response) {
 
-  const id = request.params.id;
+  const id = request.params.taskId;
  
-  response.status(200).json({
-    message: `You issued a delete request for ID: ${id}.`
+  connection.query(`DELETE FROM List_of_tasks WHERE taskId=?`, [id], function (err) {
+    if (err) {
+      response.status(500).json({error: err});
+    } else {
+      response.sendStatus(200);
+    }
   }); 
 }); 
 
 
-module.exports.app = serverlessHttp(app);
 
+module.exports.app = serverlessHttp(app);
